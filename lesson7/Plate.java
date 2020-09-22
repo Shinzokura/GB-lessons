b20da6f48120ac22f97f1686e2afcb97ddeafd95
Shinzokura <70546804+Shinzokura@users.noreply.github.com>
2020-09-22 16:26:42 +0300

    Add files via upload

---
+++ lesson7/Plate.java
@@ -0,0 +1,28 @@
+package ru.geekbrains.lesson7;
+
+public class Plate {
+    private int amountOfFood;
+
+    Plate(int amountOfFood) {
+        this.amountOfFood = amountOfFood;
+    }
+
+    @Override
+    public String toString() {
+        return "Еды в тарелке: " + amountOfFood;
+    }
+
+    void decreaseFood(int appetite) {
+        if (amountOfFood >= appetite) {
+            amountOfFood -= appetite;
+        }
+    }
+
+    int getAmountOfFood() {
+        return amountOfFood;
+    }
+
+    void increaseFood(int amount) {
+        amountOfFood += amount;
+    }
+}
